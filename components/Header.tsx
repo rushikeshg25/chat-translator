a2d48d5eb620967c41cdc1834fd3ead96f637d5f
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2023-12-03 14:37:48 +0530

    added Dark Mode Button and Mode Toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Logo from "./Logo";
+import { DarkModeToggleButton } from "./DarkModeToggleButton";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col items-center p-5 pl-2 mx-auto bg-white sm:flex-row dark:bg-gray-900 max-w-7xl">
         <Logo />
+        <div className="flex items-center justify-end flex-1 space-x-4">
+          <DarkModeToggleButton />
+        </div>
       </nav>
+      {/* Upgrade Banner */}
     </header>
   );
 };
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,27 @@
 import Link from "next/link";
 import React from "react";
+import { AspectRatio } from "./ui/aspect-ratio";
+import Image from "next/image";
+import LogoImg from "../images/logoImg.svg";
 
 const Logo = () => {
-  return <Link href="/">Logo</Link>;
+  return (
+    <Link href="/">
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio
+          ratio={16 / 9}
+          className="flex items-center justify-center"
+        >
+          <Image
+            priority
+            src={LogoImg}
+            alt="Logo"
+            className="dark:filter dark:invert"
+          />
+        </AspectRatio>
+      </div>
+    </Link>
+  );
 };
 
 export default Logo;
