dd527a65b810f6aa4185da4064357a536c9cc196
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2023-12-04 16:23:22 +0530

    feat:Session for user

--- components/User/UserNav.tsx
+++ components/User/UserNav.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   DropdownMenu,
@@ -8,12 +9,34 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import UserAvatar from "./UserAvatar";
+import { Session } from "next-auth";
+import { Button } from "../ui/button";
+import { signIn } from "next-auth/react";
 
-const UserNav = () => {
+const UserNav = ({ session }: { session: Session | null }) => {
+  if (!session) {
+    return (
+      <Button
+        onClick={() => {
+          signIn();
+        }}
+      >
+        Signin
+      </Button>
+    );
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <UserAvatar name="rushikesh" image="https://github.com/shadcn.png" />
+        {session.user && session.user.name ? (
+          <UserAvatar
+            name={session.user?.name}
+            image="https://github.com/shadcn.png"
+          />
+        ) : (
+          <UserAvatar name="User" image="User" />
+        )}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
